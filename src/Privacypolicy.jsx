ceb5ff215504ff6478dccca3f723291e67c75146
BENEDYCT <emekaossy6779@gmail.com>
2025-06-12 09:08:49 +0100

    Added Footer

--- src/Privacypolicy.jsx
+++ src/Privacypolicy.jsx
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar'
 import Hero from './components/privacypolicy/Hero'
 import Docs from './components/privacypolicy/Docs'
 import Rightsnchoices from './components/privacypolicy/Rightsnchoices'
-import Footer from './components/home/footer'
+import Footer from './components/home/Footer'
 
 const Privacypolicy = () => {
   return (
